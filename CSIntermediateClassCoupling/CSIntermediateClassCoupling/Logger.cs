a3e9d1203310a43d57553b490eb5e07ca4758592
Evan Leahy <49353272+echolimauw@users.noreply.github.com>
2021-05-06 18:02:26 -0700

    Learning about composition

---
+++ CSIntermediateClassCoupling/CSIntermediateClassCoupling/Logger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSIntermediateClassCoupling
+{
+    partial class Program
+    {
+        public class Logger
+        {
+            public void Log(string messasge)
+            {
+                Console.WriteLine(messasge);
+            }
+        }
+    }
+}
--- CSIntermediateClassCoupling/CSIntermediateClassCoupling/Program.cs
+++ CSIntermediateClassCoupling/CSIntermediateClassCoupling/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace CSIntermediateClassCoupling
+﻿namespace CSIntermediateClassCoupling
 {
     partial class Program
     {
